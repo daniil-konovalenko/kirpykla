b06b200db91a02ee04716baac1caf560aa22bcf5
Daniel Konovalenko <ddkono@gmail.com>
2015-10-20 10:45:54 +0300

    Добавил вывод таблицы в templates/results.html

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, render_template
 from service import get_results
 import sqlite3
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,18 +13,21 @@ def index():
 @app.route('/results', methods=['GET', 'POST'])
 def results():
     if request.method == 'POST':
+        logging.debug('Post sent')
+
         connection = sqlite3.connect('results.sqlite3')
         cursor = connection.cursor()
+        logging.debug(request.form)
         student_data = dict(
             first_name = request.form['first_name'],
             second_name = request.form['second_name'],
             last_name = request.form['last_name'],
-            school_id = request.form['school_id']
+            school_id = request.form['login_statgrad']
         )
-
+        logging.debug(student_data)
         results_table = get_results(student_data, cursor)
         connection.close()
-        return render_template('results.html', results_table)
+        return render_template('results.html', results_table=results_table)
 
     else:
         return render_template('results.html')
